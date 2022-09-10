d500274bbe0c98dac208000efb5ac132cf96871c
Perfect-J <johnsonayo532@gmail.com>
2022-09-09 17:56:18 -0700

    print task 2 alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,16 +14,35 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 alphabet = tolower(alphabet);
 putchar(alphabet);
-if (alphabet == 'z')
-{
-alphabet = 'A';
-for (; alphabet <= 'Z'; alphabet++)
-{
-putchar(alphabet);
-}
-break;
-}
 }
 putchar('\n');
 return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
